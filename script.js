ad6f0c9cdc1bfef3b1f6c3c19c529311cc8f8ff5
Nikhil6791 <nikhilspandey08@gmail.com>
2024-12-25 18:10:52 +0530

    Added experience page

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
   let currentslide = 0;
   const slides = document.querySelectorAll(".carousel-item");
   const totalslides = slides.length;
-  console.log(totalslides);
-  
+
   document.querySelector(".next").addEventListener("click", () => {
     slides[currentslide].classList.remove("active");
     currentslide = (currentslide + 1) % totalslides;
     slides[currentslide].classList.add("active");
   });
-  
+
   document.querySelector(".prev").addEventListener("click", () => {
     slides[currentslide].classList.remove("active");
     currentslide = (currentslide - 1 + totalslides) % totalslides;
     slides[currentslide].classList.add("active");
   });
-  
+
   const menuIcon = document.getElementById("menuIcon");
   const popupMenu = document.getElementById("popupMenu");
-  
-  menuIcon.addEventListener("click", (event) => {
-    event.preventDefault();
-    popupMenu.classList.toggle("hidden");
-  });
-});
 
+  if (popupMenu) {
+    menuIcon.addEventListener("click", (event) => {
+      event.preventDefault();
+      popupMenu.classList.toggle("hidden");
+    });
+  }
+});
